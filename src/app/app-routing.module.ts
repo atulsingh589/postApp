f437e8da1b7dee6593f14a85660930ddcffddb04
atulsingh589 <atulsingh589@gmail.com>
2021-05-26 13:20:29 +0530

    Url changed from login to /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'post', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'post', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
